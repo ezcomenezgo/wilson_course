2dd264fd34817a89eb5e99b6339482803ca88b8e
Claire Li <s3310507057@gmail.com>
2023-10-16 12:07:04 +0100

    course start

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello world");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+
+        // Press Ctrl+D to start debugging your code. We have set one breakpoint
+        // for you, but you can always add more by pressing Cmd+F8.
+    }
+}
