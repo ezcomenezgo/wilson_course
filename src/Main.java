dc934ddd7f2343890a97aa51c3c23aead9db934f
Claire Li <s3310507057@gmail.com>
2023-10-19 20:36:11 +0100

    2023/10/19 make a guess number game

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
@@ -8,15 +10,14 @@ public class Main {
         System.out.println("Hello world");
         String x = "test";
         String y = "test";
-//        System.out.println(x.equals(y));
+        System.out.println(x.equals(y));
 
         String[] array = new String[5];
 
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter 5 strings");
-        array[0]=scanner.nextLine();
-
+        array[0] = scanner.nextLine();
 
 
         System.out.println(array[0]);
@@ -29,5 +30,36 @@ public class Main {
 
         // Press Ctrl+D to start debugging your code. We have set one breakpoint
         // for you, but you can always add more by pressing Cmd+F8.
+
+        // Array
+        String[] friends = {"Abby", "Ben", "Claire"};
+
+        // 2D Array
+        // it means the outer array can put 3 arrays inside
+        // and inner arrays can put 3 integer values
+        int[][] first2DArr = new int[3][3];
+        // assign 5 to second index of second array in first2DArr
+        first2DArr[2][2] = 5;
+
+        // For loop
+        for (int i = 0; i < friends.length; i++) {
+            System.out.println(friends[i] + " is my friend");
+        }
+
+        // For loop enhance version
+        // declare a variable first (can call anything)
+        // use ":" means "in" the specific array
+        // then put the array you want to loop
+        for (String friend : friends) {
+            System.out.println(friend + " is my friend");
+        }
+
+        // while loop
+        // remember to set the stop rule
+        int i = 0;
+        while (i < 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
     }
 }
--- src/PracticeOne.java
+++ src/PracticeOne.java
@@ -1,31 +1,32 @@
 import javax.swing.*;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.Random;
 
 public class PracticeOne {
     private static final DecimalFormat DF = new DecimalFormat("00.00");
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // BMI calculate
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Type your weight(kg): ");
-//        double weight = scanner.nextDouble();
-//
-//        System.out.println("Type your height(cm): ");
-//        double height = scanner.nextDouble() / 100;
-//
-//        double bmi = Double.parseDouble(DF.format(weight / (height * height)));
-//        System.out.println("Your BMI is: " + bmi);
-//
-//        if (bmi < 18.5) {
-//            System.out.println("Too less");
-//        } else if (bmi < 24) {
-//            System.out.println("Normal");
-//        } else if (bmi < 30) {
-//            System.out.println("A little fat");
-//        } else {
-//            System.out.println("Too fat");
-//        }
+        System.out.print("Type your weight(kg): ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Type your height(cm): ");
+        double height = scanner.nextDouble() / 100;
+
+        double bmi = Double.parseDouble(DF.format(weight / (height * height)));
+        System.out.println("Your BMI is: " + bmi);
+
+        if (bmi < 18.5) {
+            System.out.println("Too less");
+        } else if (bmi < 24) {
+            System.out.println("Normal");
+        } else if (bmi < 30) {
+            System.out.println("A little fat");
+        } else {
+            System.out.println("Too fat");
+        }
 
         // Vaccine search
         String vaccine = JOptionPane.showInputDialog("Please type vaccine name");
@@ -35,12 +36,46 @@ public class PracticeOne {
             JOptionPane.showMessageDialog(null, "AZ\nProtection");
         } else if (vaccine.equals("bnt")) {
             JOptionPane.showMessageDialog(null, "BNT\nProtection");
-        } else if (vaccine.equals("modera")) {
+        } else if (vaccine.equals("Modena")) {
             JOptionPane.showMessageDialog(null, "莫德納\nProtection");
         } else if (vaccine.equals("jassen")) {
             JOptionPane.showMessageDialog(null, "嬌生\nProtection");
         } else {
             JOptionPane.showMessageDialog(null, "Type error");
+        }
+
+        // guess number
+        int start = 0;
+        int end = 100;
+        // Instance of random class
+        Random rand = new Random();
+        // Setting the upper bound to generate the random numbers in specific range
+        int upperbound = 100;
+        // Generating random values from 0 - 24
+        // using nextInt()
+        int ansNum = rand.nextInt(upperbound);
+        // if guessNum < ans
+        // guessNum -> 100
+        // if guessNum > ans
+        // 1 -> guessNun
+        while (true) {
+            System.out.print("From " + start + " to " + end + ". Type a number: ");
+            int guessNum = scanner.nextInt();
+            if (guessNum > end || guessNum < start) {
+                System.out.println("out of the range!");
+                continue;
+            }
+
+            if (guessNum == ansNum) {
+                System.out.println("You guess right!");
+                break;
+            }
+
+            if (guessNum < ansNum) {
+                start = guessNum;
+            } else {
+                end = guessNum;
+            }
         }
     }
 }
