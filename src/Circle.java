6623de1c2defba68aba3421a1afe5b884330ba4f
Claire Li <s3310507057@gmail.com>
2023-10-23 12:11:22 +0100

    2023/10/23 OOP basic knowledge

---
+++ src/Circle.java
@@ -0,0 +1,32 @@
+public class Circle {
+    // Declare attribute of Circle
+    // use 'public' make it can be used everywhere
+    // use 'private' make it only be used in this class, normally will do this action
+    // the variable declared above constructor called Fields
+    private double radius;
+    // use 'static' can avoid occupy memory space when declare many Circle objects
+    // And it belongs to class
+    private static final double PI = 3.14;
+
+    // A class can have many constructors, it uses "methods overloading" concept
+    // make codes more flexible
+    public Circle() {
+        this(1);
+    }
+
+    // Circle class's constructor
+    // The name needed to the same with the class
+    // it won't return any value
+    // it will do a new object
+    public Circle(double radius) {
+        // 'this' means the class itself
+        this.radius = radius;
+        // variable declared in the methods called local variables
+        int x = 3;
+    }
+
+    // methods normally be set in 'public'
+    public double getArea() {
+        return Circle.PI * radius * radius;
+    }
+}
---
+++ src/OopPractice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OopPractice {
+    public static void main(String[] args) {
+        // declare circle1 type is Circle, it will refer to Circle.java
+        // create a new Circle object
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle();
+        // Object-oriented programming
+        System.out.println(circle1.getArea());
+        System.out.println(circle2.getArea());
+
+        // Execute Scanner class's constructor
+        // Then can use the methods in Scanner class like s.nextDouble()
+        Scanner s = new Scanner(System.in);
+    }
+}
